c4b63103bcdb4774585ab1de583ca9e14713d960
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2024-06-08 14:29:43 +0200

    Created Entity class Ingredient

---
+++ app/src/main/java/com/example/marmitonai/Models/Ingredient.kt
@@ -0,0 +1,15 @@
+package com.example.marmitonai.Models
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ingredients")
+data class Ingredient(
+    @PrimaryKey val barcode: String,
+    val servingSize: Int,
+    val servingUnit: String,
+    val kcal: Int, val fat: Int,
+    val carbs: Int,
+    val sugars: Int,
+    val protein: Int,
+    val salt: Int,
+    val imageId: String)
